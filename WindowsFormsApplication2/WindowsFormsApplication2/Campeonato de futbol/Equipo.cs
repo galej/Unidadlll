58995216c4e7e2870b3007b2907df0f8850a7f13
LAB02-06 <LAB02-06@DESKTOP-7KA7BRS>
2019-03-07 13:59:12 -0600

    Se agregaron otras valiables a las mismas y falta partido, campeonato.

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Campeonato de futbol/Equipo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2.Campeonato_de_futbol
+{
+    class Equipo
+    {
+        private int _id;
+        private string _nombre;
+        private string _comunidad;
+        private string _entrenador;
+
+        public int Id
+        {
+            get
+            {
+                return this._id;
+            }
+            set
+            {
+                if(value >= 0)
+                {
+                    this._id = value;
+                }
+            }
+        }
+        public string Nombre
+        {
+            get
+            {
+                return this._nombre;
+            }
+            set
+            {
+                if (value != null || value.Length >= 0 && value.Length <= 30)
+                {
+                    this._nombre = value;
+                }
+            }
+        }
+        public string Comunidad
+        {
+            get
+            {
+                return this._comunidad;
+            }
+            set
+            { 
+                if (value != null || value.Length >=0 && value.Length <= 30)
+                {
+                    this._comunidad = value;
+                }
+            }
+        }
+        public string Entrenador
+        {
+            get
+            {
+                return this._entrenador;
+            }
+            set
+            {
+                if (value != null || value.Length>=0 && value.Length <= 30)
+                {
+                    this._entrenador = value; 
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Campeonato de futbol/Jugador.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2.Campeonato_de_futbol
+{
+    class Jugador
+    {
+        private int _id;
+        private string _nombre;
+        private string _apellidos;
+        private DateTime _fechaNacimiento;
+        private byte _numero;
+        private Equipo _idEquipo;
+
+        public Jugador()
+        {
+            this._nombre = "Papulince";
+            this._apellidos = "Del campo";
+            this._fechaNacimiento = DateTime.Today;
+        }
+        public int Id
+        {
+            get
+            {
+                return this._id;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this._id = value;
+                }
+            }
+        }
+        public string Nombre
+        {
+            get
+            {
+                return this._nombre;
+            }
+            set
+            {
+                if (value != null || value.Length >= 0 && value.Length <= 30)
+                {
+                    this._nombre = value;
+                }
+            }
+        }
+        public string Apellidos
+        {
+            get
+            {
+                return this._apellidos;
+            }
+            set
+            {
+                if (value != null || value.Length >= 0 && value.Length <= 30)
+                {
+                    this._apellidos = value;
+                }
+            }
+        }
+        public DateTime FechaNacimiento
+        {
+            get
+            {
+                return this._fechaNacimiento;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    this._fechaNacimiento = value;
+                }
+            }
+        }
+        public Equipo IdEquipo {
+            get
+            {
+                return this._idEquipo;
+            }
+            set {
+                if (value != null || ((Equipo)value).Nombre.ToLower().Equals("llanito"))
+                {
+                    this._idEquipo = value;
+                }
+            }
+        }
+    }
+}
