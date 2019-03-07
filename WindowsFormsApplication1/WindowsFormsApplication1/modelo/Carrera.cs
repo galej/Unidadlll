a38f187a3d13f6c0996dbd9828284ed1867fb4c7
Gabriel Alejandro <48102422+galej@users.noreply.github.com>
2019-03-07 12:43:41 -0600

    Update Carrera.cs

--- WindowsFormsApplication1/WindowsFormsApplication1/modelo/Carrera.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/modelo/Carrera.cs
@@ -8,7 +8,18 @@ namespace WindowsFormsApplication1.modelo
 {
     class Carrera
     {
-        public string nombre { get; set; }
+        private string _nombre;
+        private float _duracion;
+        private string _descrip;
+        public string nombre { get
+        {
+                retun this._nombre;
+        }
+            set
+            {
+                this._nombre;
+            }
+                             }
         public float duracion { get; set; }
         public string descrip { get; set; }
     }
