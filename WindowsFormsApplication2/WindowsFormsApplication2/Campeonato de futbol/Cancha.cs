7f1fb01f92aa060b70c1090f79d109487f043e62
LAB02-06 <LAB02-06@DESKTOP-7KA7BRS>
2019-03-07 13:22:34 -0600

    hola

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Campeonato de futbol/Cancha.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2.Campeonato_de_futbol
+{
+    class Cancha
+    {
+        private int _id;
+        private string _nombre;
+        private string _ubicacion;
+
+        public int Id
+        {
+            get
+            {
+                return this._id;
+            }
+            set
+            {
+                if(value > 0)
+                {
+                    this._id = value;
+                }
+            }
+
+        }
+        public string Nombre
+        {
+            get
+            {
+                return this._nombre;
+            }
+            set
+            {
+                if(value != null || value.Length > 0 && value.Length <=30)
+                {
+                    this._nombre = value;
+                }
+            }
+
+        }
+        public string Ubicacion
+        {
+            get
+            {
+                return this._ubicacion;
+            }
+            set
+            {
+                if(value != null || value.Length > 0 && value.Length <= 30)
+                {
+                    this._ubicacion = value;
+                }
+            }
+        }
+    }
+}
